340b4393c6a9d8a48ff98573e627312ccbcc3260
DevVladikNT <trycatchernt@gmail.com>
2024-11-14 23:42:46 +0300

    v0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # TypeStatsNT
+
 Yet another utility program for collecting data on keyboard usage.
+
+## Description
+
+This program counts how many times keys on your keyboard were pressed and saves the result in JSON files locally.
+Don't worry, no data is sent to the FBI, it's just for your personal use xD.
+
+After a few hours, you can see the statistics:
+
+* Most used keys overall (including `Enter`, `Ctrl`, etc.).
+* Letter sequences (for example, after pressing `i`, there's a 24.19% chance of pressing `n` for me).
+
+> [!WARNING]
+> Keep in mind that I'm not sure whether anti-cheat systems can detect this program,
+so I think you should turn it off if you're worried. If you have any other information, please let me know.
+
+## How to use
+
+At first you must have Python installed. Install dependencies and run `main.py`.
+
+```
+pip install -r requirements.txt
+python main.py
+```
+
+## About
+
+You can find me on [Telegram](https://t.me/VladikNT).
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import json
 import os
 import time
-import json
 
 import keyboard
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 
 def stats_func(e: keyboard.KeyboardEvent) -> None:
@@ -30,6 +30,24 @@ def debug_func(e: keyboard.KeyboardEvent) -> None:
     print(f'[{e.name}] > [{key_map.get(str(e.scan_code), "unknown")}] {e.scan_code}')
 
 
+def compare_versions() -> None:
+    global config
+    last_ver = config.get('ver', '0.0.0')
+    old_parts = last_ver.split('.')
+    new_parts = VERSION.split('.')
+    for i in range(3):
+        if new_parts[i] > old_parts[i]:
+            update()
+            return
+
+
+def update() -> None:
+    config['ver'] = VERSION
+    # todo for future updates
+    with open('data/config.json', 'w') as config_file:
+        config_file.write(json.dumps(config))
+
+
 def format_counter(count: int) -> str:
     if 0 <= count < 1e3:
         result = str(count)
@@ -47,34 +65,63 @@ if __name__ == '__main__':
     key_stats = {}
     key_pairs = {}
     last = None
-    with open('map.json', 'r') as file:
+    with open('maps/map.json', 'r') as file:
         key_map = json.loads(file.read())
-    if os.path.isfile('stats.json'):
-        with open('stats.json', 'r') as file:
+    if os.path.isfile('data/stats.json'):
+        with open('data/stats.json', 'r') as file:
             key_stats = json.loads(file.read())
-    if os.path.isfile('pairs.json'):
-        with open('pairs.json', 'r') as file:
+    if os.path.isfile('data/pairs.json'):
+        with open('data/pairs.json', 'r') as file:
             key_pairs = json.loads(file.read())
 
+    if not os.path.isfile('data/config.json'):
+        config = {'ver': VERSION}
+        print('Welcome! I hope you will find it useful :D\n'
+              'Do you use RU keyboard? y/n')
+        key = keyboard.read_key()
+        print()
+        config['ru'] = 1 if keyboard.key_to_scan_codes(key)[0] == 21 else 0
+        with open('data/config.json', 'w') as config_file:
+            config_file.write(json.dumps(config))
+    else:
+        try:
+            with open('data/config.json', 'r') as file:
+                config = json.loads(file.read())
+            compare_versions()
+        except json.JSONDecodeError:
+            os.remove('data/config.json')
+            print('Corrupted config!\nExiting...')
+            time.sleep(1)
+            exit()
+
+    if config['ru'] == 1:
+        with open('maps/map_ru.json', 'r', encoding='utf-8') as file:
+            key_map_ru = json.loads(file.read())
+        for key in key_map_ru.keys():
+            key_map[key] += key_map_ru[key]
+
+    menu = [
+        '[1] Collect data',
+        '[2] Most used keys',
+        '[3] Deep letters analysis',
+        '[4] Check your keyboard',
+        '[9] Clear settings and exit',
+        '[0] Clear data',
+        'Press any other key to exit',
+    ]
     while True:
-        menu = [
-            '1 - Collect data',
-            '2 - Most used keys',
-            '3 - Check your keyboard',
-            '0 - Clear data',
-            'Press any other key to exit',
-        ]
         print('\n'.join(menu), '\n')
 
+        time.sleep(0.1)
         key = keyboard.read_key(True)
         if key == '1':
-            print('Just use your PC as usual...')
+            print('Just use your PC as usual...\n(But don\'t close this program)')
             keyboard.on_release(stats_func)
             while True:
-                time.sleep(10)
-                with open('stats.json', 'w') as file:
+                time.sleep(60)
+                with open('data/stats.json', 'w') as file:
                     file.write(json.dumps(key_stats))
-                with open('pairs.json', 'w') as file:
+                with open('data/pairs.json', 'w') as file:
                     file.write(json.dumps(key_pairs))
         elif key == '2':
             top_size = 10 if len(key_stats.keys()) > 10 else len(key_stats.keys())
@@ -86,19 +133,51 @@ if __name__ == '__main__':
                 print(f'Top-{top_size}:')
                 for sign in list(stats_sorted.keys())[:top_size]:
                     sign_rate = stats_sorted[sign] / sum(stats_sorted.values())
-                    print(f'{sign_rate*100:.2f}%\t{key_map[str(sign)]}')
+                    print(f'{sign_rate*100:.2f}%\t[{key_map[str(sign)]}]')
         elif key == '3':
+            keys_to_ignore = ['26', '27', '39', '40'] if config['ru'] == 0 else []
+            key_usage_count = {}
+            nearest_neighbors = {}
+            for key in key_pairs.keys():
+                keys = key.split('_')
+                if (keys[0] in keys_to_ignore) or (keys[1] in keys_to_ignore):
+                    continue
+                key_usage_count[keys[0]] = key_usage_count.get(keys[0], 0) + key_pairs[key]
+                key_usage_count[keys[1]] = key_usage_count.get(keys[1], 0) + key_pairs[key]
+                neighbors = nearest_neighbors.get(keys[0], {})
+                neighbors[keys[1]] = neighbors.get(keys[1], 0) + key_pairs[key]
+                nearest_neighbors[keys[0]] = neighbors
+
+            print('All characters rate:')
+            key_usage_count = dict(sorted(key_usage_count.items(), key=lambda item: item[1], reverse=True))
+            for key in key_usage_count.keys():
+                key_rate = key_usage_count[key] / sum(key_usage_count.values())
+                print(f'{key_rate * 100:.2f}% [{key_map[str(key)]}]   >>>', end='   ')
+                nearest_neighbors[key] = dict(
+                    sorted(nearest_neighbors[key].items(), key=lambda item: item[1], reverse=True))
+                for neighbor in nearest_neighbors[key].keys():
+                    neighbor_rate = nearest_neighbors[key][neighbor] / sum(nearest_neighbors[key].values())
+                    print(f'{neighbor_rate * 100:.2f}% [{key_map[str(neighbor)]}]', end=', ')
+                print()
+        elif key == '4':
             print('Press Ctrl + Backspace to exit')
             keyboard.on_press(debug_func)
             keyboard.wait('ctrl+backspace')
             time.sleep(0.5)
+        elif key == '9':
+            try:
+                os.remove('data/config.json')
+                print('Success!\nExiting...')
+            except FileNotFoundError:
+                print('No config file detected.\nExiting...')
+            time.sleep(1)
+            exit()
         elif key == '0':
             try:
-                os.remove('stats.json')
-                os.remove('pairs.json')
+                os.remove('data/stats.json')
+                os.remove('data/pairs.json')
                 print('Data has been cleared!')
             except FileNotFoundError:
                 print('No data detected.')
         else:
             exit()
-        time.sleep(0.1)
